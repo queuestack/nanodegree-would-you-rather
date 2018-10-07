4645afacb71a6bdce0d64b130f30a585a5bb3d38
SeongkyuLee <seongkyu0617@gmail.com>
2018-10-07 17:52:54 +0900

    style: add semantic-ui to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { setAuthedUser } from '../store/actions/authedUser'
-import { Image, List } from 'semantic-ui-react';
+import { Header, Image, Card, Segment } from 'semantic-ui-react';
 
 class Login extends Component {
     handleLogin(e, id) {
@@ -20,25 +20,30 @@ class Login extends Component {
         }
 
         return (
-            <List selection verticalAlign='middle'>
-                {Object.keys(users).map(user => {
-                    const { name, id, avatarURL } = users[user]
-                    return (
-                        <List.Item 
-                            key={name}
-                        >
-                            <List.Content>
-                                <Image avatar src={avatarURL}/>
-                                <List.Header
-                                    onClick={e => this.handleLogin(e, id)}
-                                >
-                                    {name}
-                                </List.Header>
-                            </List.Content>
-                        </List.Item>
-                    )
-                })}
-            </List>
+            <Segment>
+                <Header>
+                    Please Sign In To Continue
+                </Header>
+                <Card.Group selection verticalAlign='middle'>
+                    {Object.keys(users).map(user => {
+                        const { name, id, avatarURL } = users[user]
+                        return (
+                            <Card>
+                                <Card.Content>
+                                    <Image floated='right' avatar src={avatarURL}/>
+                                    <Card.Header
+                                        onClick={e => this.handleLogin(e, id)}
+                                    >
+                                        {name}
+                                    </Card.Header>
+                                </Card.Content>                         
+                            </Card>
+
+                        )
+                    })}
+                </Card.Group>
+            </Segment>
+
         )
     }
 }
