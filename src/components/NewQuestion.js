aaa34490aa31cb94cc52a3cafa35a59fec860bc4
SeongkyuLee <seongkyu0617@gmail.com>
2018-10-07 17:52:33 +0900

    style: add semantic-ui to new question

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { handleAddQuestion } from '../store/actions/questions'
+import { Card, Divider, Form, Button } from 'semantic-ui-react'
 
 class NewQuestion extends Component {
     state = {
@@ -43,27 +44,43 @@ class NewQuestion extends Component {
         }
 
         return (
-            <div>
-                <h3>New Question</h3>
-                <form className='new-question' onSubmit={this.handleSubmit}>
-                    <textarea
-                        placeholder=''
-                        value={optionOneText}
-                        onChange={this.handleOptionOneChange}
-                    />
-                    <textarea
-                        placeholder=''
-                        value={optionTwoText}
-                        onChange={this.handleOptionTwoChange}
-                    /> 
-                    <button
-                        type='submit'
-                        disabled={optionOneText === '' || optionTwoText === ''}
-                    >
-                        Submit
-                    </button>                   
-                </form>
-            </div>
+            <Card>
+                <Card.Content>
+                    <Card.Header>
+                        Create New Question
+                    </Card.Header>
+                </Card.Content>
+
+                <Card.Content>
+                    <Form className='new-question' onSubmit={this.handleSubmit}>
+                        <Form.Field>
+                            <input
+                                placeholder='Enter Option One Text Here'
+                                value={optionOneText}
+                                onChange={this.handleOptionOneChange}
+                            />                        
+                        </Form.Field>
+
+                        <Divider horizontal>OR</Divider>
+
+                        <Form.Field>
+                            <input
+                                placeholder='Enter Option Two Text Here'
+                                value={optionTwoText}
+                                onChange={this.handleOptionTwoChange}
+                            /> 
+                        </Form.Field>
+
+                        <Button
+                            type='submit'
+                            disabled={optionOneText === '' || optionTwoText === ''}
+                        >
+                            Submit
+                        </Button>                   
+                    </Form>
+                </Card.Content>
+
+            </Card>
         )
     }
 }
