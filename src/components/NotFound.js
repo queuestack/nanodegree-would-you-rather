9b9d66631dc9a6652422c6c4399ef4c3d8ba2ec8
SeongkyuLee <seongkyu0617@gmail.com>
2018-10-25 23:52:13 +0900

    refac: modify NotFound component to stateless component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 
-class NotFound extends Component {
-    render() {
-        return (
-            <div>
-                404 Not Found
-                <Link to="/">
-                    <div>
-                        Go Back Home
-                    </div>
-                </Link>                      
-            </div>
-      
-        )
-    }
+function NotFound(props) {
+    return (
+        <div>
+            404 Not Found
+            <Link to="/">
+                <div>
+                    Go Back Home
+                </div>
+            </Link>                      
+        </div>
+    )
 }
 
 export default NotFound
