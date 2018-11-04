75ef9687a3c3a2b5aa8b3a281558cc4c46ccb74e
SeongkyuLee <seongkyu0617@gmail.com>
2018-11-04 22:18:08 +0900

    fix: Redirect to login page when it's Not Found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 function NotFound(props) {
     return (
         <div>
             404 Not Found
-            <Link to="/">
-                <div>
-                    Go Back Home
-                </div>
-            </Link>                      
+            <Redirect to="/" />                      
         </div>
     )
 }
